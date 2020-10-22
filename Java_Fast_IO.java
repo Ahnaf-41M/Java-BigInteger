e67f3b2ed85387a5dd85e63b637ca6ffc232ea17
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2020-10-22 18:59:52 +0600

    Files

--- Java_Fast_IO.java
+++ Java_Fast_IO.java
@@ -10,15 +10,15 @@ public class Java_Fast_IO {
 		StringTokenizer st;
 
 		public FastReader() {
-			br = new BufferedReader(new
-			                        InputStreamReader(System.in));
+			br = new BufferedReader(new InputStreamReader(System.in));
 		}
 
 		String next() {
 			while (st == null || !st.hasMoreElements()) {
 				try {
 					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
+				} 
+				catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
@@ -41,13 +41,13 @@ public class Java_Fast_IO {
 			String str = "";
 			try {
 				str = br.readLine();
-			} catch (IOException e) {
+			} 
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 			return str;
 		}
 	}
-
 	public static void main(String[] args) {
 		FastReader s = new FastReader();
 		int n = s.nextInt();
